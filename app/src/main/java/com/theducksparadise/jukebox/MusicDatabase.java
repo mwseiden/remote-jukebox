065ee69e5b25468ae42b7b2ddab55deef3462339
Mark Seiden <mseiden@skillz.com>
2015-04-12 15:29:15 -0400

    Load and save tags from genres

--- app/src/main/java/com/theducksparadise/jukebox/MusicDatabase.java
+++ app/src/main/java/com/theducksparadise/jukebox/MusicDatabase.java
@@ -23,6 +23,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
@@ -31,7 +32,8 @@ public class MusicDatabase extends SQLiteOpenHelper {
 
     private static final int DATABASE_VERSION_001 = 2;
     private static final int DATABASE_VERSION_002 = 3;
-    private static final int DATABASE_VERSION = DATABASE_VERSION_002;
+    private static final int DATABASE_VERSION_003 = 4;
+    private static final int DATABASE_VERSION = DATABASE_VERSION_003;
 
     private static final String DATABASE_NAME = "jukebox_music";
 
@@ -39,6 +41,8 @@ public class MusicDatabase extends SQLiteOpenHelper {
     private static final String ALBUM_TABLE_NAME = "album";
     private static final String SONG_TABLE_NAME = "song";
     private static final String QUEUE_TABLE_NAME = "queue";
+    private static final String TAG_TABLE_NAME = "tag";
+    private static final String TAG_SONG_TABLE_NAME = "tag_song";
 
     private static final String ARTIST_TABLE_CREATE =
             "CREATE TABLE " + ARTIST_TABLE_NAME + " (" +
@@ -68,10 +72,25 @@ public class MusicDatabase extends SQLiteOpenHelper {
                     "sequence INTEGER, " +
                     "FOREIGN KEY(song_id) REFERENCES " + SONG_TABLE_NAME + "(id));";
 
+    private static final String TAG_TABLE_CREATE =
+            "CREATE TABLE " + TAG_TABLE_NAME + " (" +
+                    "id INTEGER PRIMARY KEY, " +
+                    "name TEXT;";
+
+    private static final String TAG_SONG_TABLE_CREATE =
+            "CREATE TABLE " + TAG_SONG_TABLE_NAME + " (" +
+                    "id INTEGER PRIMARY KEY, " +
+                    "tag_id INTEGER, " +
+                    "song_id INTEGER, " +
+                    "FOREIGN KEY(tag_id) REFERENCES " + TAG_TABLE_NAME + "(id)), " +
+                    "FOREIGN KEY(song_id) REFERENCES " + SONG_TABLE_NAME + "(id));";
+
     private List<Artist> artists;
 
     private Map<String, Artist> artistIndex;
     private Map<Integer, Song> songIndex;
+
+    private Map<String, List<Song>> tags;
 
     private static volatile MusicDatabase instance;
 
@@ -98,12 +117,17 @@ public class MusicDatabase extends SQLiteOpenHelper {
         db.execSQL(ALBUM_TABLE_CREATE);
         db.execSQL(SONG_TABLE_CREATE);
         db.execSQL(QUEUE_TABLE_CREATE);
+        db.execSQL(TAG_TABLE_CREATE);
+        db.execSQL(TAG_SONG_TABLE_CREATE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        if (oldVersion == DATABASE_VERSION_001) {
+        if (oldVersion < DATABASE_VERSION_002) {
             db.execSQL(QUEUE_TABLE_CREATE);
+        } else if (oldVersion < DATABASE_VERSION_003) {
+            db.execSQL(TAG_TABLE_CREATE);
+            db.execSQL(TAG_SONG_TABLE_CREATE);
         }
     }
 
@@ -184,6 +208,7 @@ public class MusicDatabase extends SQLiteOpenHelper {
         artists = new ArrayList<>();
         artistIndex = new HashMap<>();
         songIndex = new HashMap<>();
+        tags = new LinkedHashMap<>();
 
         Cursor cursor = getReadableDatabase().rawQuery(
                 "SELECT id, name FROM " + ARTIST_TABLE_NAME + " ORDER BY name",
@@ -207,6 +232,8 @@ public class MusicDatabase extends SQLiteOpenHelper {
         for (Artist artist: artists) {
             loadAlbums(artist);
         }
+
+        loadTags();
     }
 
     private void loadAlbums(Artist artist) {
@@ -262,6 +289,44 @@ public class MusicDatabase extends SQLiteOpenHelper {
         }
 
         cursor.close();
+    }
+
+    private void loadTags() {
+        Map<String, Integer> tagIds = new LinkedHashMap<>();
+
+        Cursor cursor = getReadableDatabase().rawQuery(
+                "SELECT id, name FROM " + TAG_TABLE_NAME + " ORDER BY name",
+                null
+        );
+
+        if (cursor.moveToFirst()) {
+            do {
+                tagIds.put(cursor.getString(1), cursor.getInt(0));
+                tags.put(cursor.getString(1), new ArrayList<Song>());
+            } while (cursor.moveToNext());
+        }
+
+        cursor.close();
+
+        loadTagSongs(tagIds);
+    }
+
+    private void loadTagSongs(Map<String, Integer> tagIds) {
+        for (Map.Entry<String, Integer> entry : tagIds.entrySet()) {
+            Cursor cursor = getReadableDatabase().rawQuery(
+                    "SELECT song_id FROM " + TAG_SONG_TABLE_NAME + " WHERE tag_id = ?",
+                    new String[]{entry.getValue().toString()}
+            );
+
+            if (cursor.moveToFirst()) {
+                do {
+                    tags.get(entry.getKey()).add(songIndex.get(cursor.getInt(0)));
+                } while (cursor.moveToNext());
+            }
+
+            cursor.close();
+        }
+
     }
 
     // TODO: Refactor this so it isn't so redundant
@@ -348,9 +413,16 @@ public class MusicDatabase extends SQLiteOpenHelper {
         if (asyncProgress != null) asyncProgress.updateProgress("Clearing Artists");
         getWritableDatabase().execSQL("DELETE FROM " + ARTIST_TABLE_NAME + ";");
 
+        if (asyncProgress != null) asyncProgress.updateProgress("Clearing Song Tags");
+        getWritableDatabase().execSQL("DELETE FROM " + TAG_SONG_TABLE_NAME + ";");
+
+        if (asyncProgress != null) asyncProgress.updateProgress("Clearing Tags");
+        getWritableDatabase().execSQL("DELETE FROM " + TAG_TABLE_NAME + ";");
+
         artists.clear();
         artistIndex.clear();
         songIndex.clear();
+        tags.clear();
     }
 
     private void rebuildDatabase(String path, AsyncProgress asyncProgress) {
@@ -399,6 +471,23 @@ public class MusicDatabase extends SQLiteOpenHelper {
                 album.getSongs().add(song);
                 songIndex.put(song.getId(), song);
 
+                String genre = metadata.getGenre();
+
+                if (genre == null || genre.equals("")) genre = "Unknown";
+
+                String[] songTags = genre.split(";");
+
+                for (String tag : songTags) {
+                    List<Song> taggedSongs = tags.get(tag);
+
+                    if (taggedSongs == null) {
+                        taggedSongs = new ArrayList<>();
+                        tags.put(tag, taggedSongs);
+                    }
+
+                    taggedSongs.add(song);
+                }
+
             } catch (IOException | ArrayIndexOutOfBoundsException e) {
                 // Well that sucks. Ignore it.
             }
@@ -420,6 +509,17 @@ public class MusicDatabase extends SQLiteOpenHelper {
                 }
             }
         }
+
+        for (Map.Entry<String, List<Song>> entry : tags.entrySet()) {
+            asyncProgress.updateProgress("Saving Tag " + entry.getKey());
+
+            Integer tagId = saveTag(entry.getKey());
+
+            for (Song song : entry.getValue()) {
+                saveTagSong(tagId, song);
+            }
+        }
+
     }
 
     private void saveArtist(Artist artist) {
@@ -464,7 +564,31 @@ public class MusicDatabase extends SQLiteOpenHelper {
     private void saveQueueItem(Song song, Integer sequence) {
         getWritableDatabase().execSQL(
                 "INSERT INTO " + QUEUE_TABLE_NAME + " (song_id, sequence) VALUES (?, ?);",
-                new Object[]{song.getId(), sequence}
+                new Object[]{ song.getId(), sequence }
+        );
+    }
+
+    private Integer saveTag(String name) {
+        getWritableDatabase().execSQL(
+                "INSERT INTO " + TAG_TABLE_NAME + " (name) VALUES (?);",
+                new Object[]{ name }
+        );
+
+        Integer result = null;
+
+        Cursor cursor = getWritableDatabase().rawQuery("SELECT last_insert_rowid() FROM " + TAG_TABLE_NAME + ";", null);
+
+        if (cursor.moveToFirst()) result = cursor.getInt(0);
+
+        cursor.close();
+
+        return result;
+    }
+
+    private void saveTagSong(Integer tagId, Song song) {
+        getWritableDatabase().execSQL(
+                "INSERT INTO " + TAG_SONG_TABLE_NAME + " (tag_id, song_id) VALUES (?, ?);",
+                new Object[]{tagId, song.getId()}
         );
     }
 
