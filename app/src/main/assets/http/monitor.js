561bfff3ac640ce84368b70847d08a60cd4dd34d
Mark Seiden <mwseiden@gmail.com>
2017-01-03 09:01:24 -0500

    Externalize CSS, add always_on option, bug fixes

--- app/src/main/assets/http/monitor.js
+++ app/src/main/assets/http/monitor.js
@@ -1,17 +1,23 @@
-angular.module('monitor', [])
-.controller('status', function($scope, $http, $interval) {
+var app = angular.module('monitor', [])
+.controller('status', function($scope, $http, $interval, $location) {
+    $scope.always_on = $location.search().always_on;
     $scope.display_ticks = 0;
     $scope.song_id = 0;
     $scope.song = null;
 
     $interval(function() {
-        if ($scope.display_ticks > 0) $scope.display_ticks--;
+        if ($scope.always_on != "1" && $scope.display_ticks > 0) $scope.display_ticks--;
         $http.get('api/v1/status').
             then(function(response) {
                 if (response.data.id != $scope.song_id) {
+                    $scope.song_id = response.data.id
                     $scope.display_ticks = 5;
                     $scope.song = response.data;
                 }
             });
     }, 2000);
 });
+
+app.config(['$locationProvider', function($locationProvider) {
+    $locationProvider.html5Mode(true);
+}]);
